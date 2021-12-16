3d86ab86b0bb72114cd4526ba858781406eafa23
Bhupendra2409 <bmalakar441@gmail.com>
2021-12-16 18:27:53 +0530

    folder structure

--- src/client/components/Login.js
+++ src/client/components/Login.js
@@ -27,7 +27,7 @@ export default function Login() {
             </div>
             {status==='Login' && <Loginform />}
             {status==='Signup' && <Signupform/>}
-        </div>
+            </div>
         </div>
     )
 }
